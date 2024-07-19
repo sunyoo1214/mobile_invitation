8438a3fd48ff00796b9cf524a5a01e96d428bd84
sunyoo1214 <sunyoo1214@gmail.com>
2024-07-20 02:07:40 +0900

    모바일 청첩장 화면

--- my-app/src/Common.js
+++ my-app/src/Common.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './Common.css';
+import './index.css';
 
 function Common() {
   return (
-    <div className="Common">
-      <header className="Common-header">
-        <img src={logo} className="Common-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="Common-Title">
+      <div className = "Common-menu"> </div>
+      <div className ="Common-margin"/>
+        <div className ="Common-date">
+          <p>2024 / 12 / 21</p> 
+          <div>S A T U R D A Y</div> 
+        </div>
+        
+      
+       <header className="Common-greeting">
+        <p>민기♥선우 결혼식에 초대합니다</p>
+        
         <a
           className="Common-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          방명록 쓰기
         </a>
-      </header>
+      </header> 
     </div>
   );
 }
